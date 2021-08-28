fbe6d61b53690dd9c8369f7ceb140ada46a08c0c
ZDubeau <zdubeau@dubeaul.com>
2021-08-28 10:20:16 +0200

    test

---
+++ App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Home from "./components/Home";
+import BasicScreen from "./components/BasicScreen";
+import DateTimeScreen from "./components/DateTimeScreen";
+import ImageScreen from "./components/ImageScreen";
+
+export default function App() {
+  return <BasicScreen />;
+}
